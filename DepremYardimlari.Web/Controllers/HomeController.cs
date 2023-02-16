d52e9c75254828e548ec65c46dd0a7f7b6588ff4
Ahmet Oğuz Koca <aoguzkoca@hotmail.com>
2023-02-16 11:49:36 +0300

    Add project files.

---
+++ DepremYardimlari.Web/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using DepremYardimlari.Web.Models;
+using DepremYardimlari.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DepremYardimlari.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Contact(string name, string company, string email, string message, string messageFrom)
+        {
+            // accountService.SaveContactData(name, company, email, message,Request.ServerVariables["remote_addr"],messageFrom);
+            MailService.SendEmail("oguz@quikmotion.com", "AidTurkey Contact Request",
+                "Name:" + name + "<br>" +
+                 "Email:" + email + "<br>" +
+                "Company:" + company + "<br>" +
+                "Message:" + message + "<br>" +
+                "Message from:" + messageFrom
+
+                );
+            return Json(new { success = true });
+        }
+    }
+}
---
+++ DepremYardimlari.Web/Services/MailService.cs
@@ -0,0 +1,63 @@
+﻿using MailKit.Security;
+using MimeKit;
+
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DepremYardimlari.Web.Services
+{
+    public class MailService
+    {
+
+        public static bool SendEmail(string emailTo, string msgSubject, string msgBody)
+        {
+            try
+            {
+                var email = "oguz@quikmotion.com";
+
+                var message = new MimeMessage();
+
+                message.From.Add(new MailboxAddress("QuikMotion", email));
+                message.To.Add(new MailboxAddress("", emailTo));
+
+                message.Subject = msgSubject;
+
+                message.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = msgBody };
+
+                using (var client = new MailKit.Net.Smtp.SmtpClient())
+                {
+                    client.ServerCertificateValidationCallback = CallBack;
+                    client.Connect(
+             "smtp.gmail.com"
+           , 587
+           , SecureSocketOptions.StartTlsWhenAvailable
+        );
+                    // client.AuthenticationMechanisms.Remove("XOAUTH2"); // Must be removed for Gmail SMTP
+                    client.Authenticate(email, "");
+
+                    client.Send(message);
+
+
+                    client.Disconnect(true);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+
+
+
+
+        }
+
+
+
+        private static bool CallBack(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+    }
+}
